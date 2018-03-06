05cbca50d190183c90dc2d57a638fcf099160b89
liji222 <502822435@qq.com>
2018-03-06 23:17:14 +0800

    element-demo

---
+++ elem/README.md
@@ -0,0 +1,4 @@
+# element-ui
+
+el-radio中可以设置disabled，则禁用,return控制选中状态
+el-xxx标签中如果没有内容，label属性可以充当内容
---
+++ elem/src/components/MyElement.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="demo">
+        <div class="demo-list">
+            <h4>单选按钮</h4>
+            <div class="demo-item">
+                <el-radio v-model="radio" label="1">选中</el-radio>
+                <el-radio v-model="radio" label="2">备选</el-radio>
+                <el-radio disabled v-model="radio1" label="禁用">禁用</el-radio>
+                <el-radio disabled v-model="radio1" label="选中且禁用">选中且禁用</el-radio>
+            </div>
+        </div>
+
+        <div class="demo-list">
+            <h4>单选按钮组</h4>
+            <div class="demo-item">
+                <el-radio-group v-model="radio2">
+                    <el-radio :label="3">备选项</el-radio>
+                    <el-radio :label="6">备选项</el-radio>
+                    <el-radio :label="9">备选项</el-radio>
+                </el-radio-group>
+            </div>
+        </div>
+
+        <div class="demo-list">
+            <h4>按钮组</h4>
+            <div class="demo-item">
+                <el-radio-group v-model="radio3" size="medium">
+                    <el-radio-button label="上海"></el-radio-button>
+                    <el-radio-button label="北京"></el-radio-button>
+                    <el-radio-button label="广州"></el-radio-button>
+                    <el-radio-button label="深圳"></el-radio-button>
+                </el-radio-group>
+            </div>
+        </div>
+
+        <div class="demo-list">
+            <h4>checkbox</h4>
+            <div class="demo-item">
+                <el-checkbox v-model="checked">备选项</el-checkbox>
+            </div>
+        </div>
+
+        <div class="demo-list">
+            <h4>多选框组</h4>
+            <div class="demo-item">
+                <el-checkbox-group v-model="checkList">
+                    <el-checkbox label="复选框 A"></el-checkbox>
+                    <el-checkbox label="复选框 B"></el-checkbox>
+                    <el-checkbox label="复选框 C"></el-checkbox>
+                    <el-checkbox label="禁用" disabled></el-checkbox>
+                    <el-checkbox label="选中且禁用" disabled></el-checkbox>
+                </el-checkbox-group>
+            </div>
+        </div>
+
+        <div class="demo-list">
+            <h4>输入框</h4>
+            <div class="demo-item">
+                <el-input v-model="input" placeholder="请输入内容"></el-input>
+            </div>
+        </div>
+
+        <div class="demo-list">
+            <h4>单选按钮</h4>
+            <div class="demo-item">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                radio: '1',
+                radio1: '选中且禁用',
+                radio2: '3',
+                radio3: '上海',
+                checked: true,
+                checkList: ['选中且禁用','复选框 A'],
+                input: '龙珠激斗'
+            };
+        }
+    }
+</script>
+
+<style>
+    .demo{
+        text-align: left;
+    }
+    .demo .demo-list{
+        margin: 20px 20px 20px 0px;
+    }
+    .demo .demo-list h4{
+        margin: 10px 0;
+    }
+    .demo .demo-item{
+        margin: 0 0 0 40px;
+    }
+</style>
